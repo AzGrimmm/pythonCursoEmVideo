c7e0b2b4a74fef3dcf10a1ee03c6ea88a04a1da6
Bruno H C Jesus <149212642+AzGrimmm@users.noreply.github.com>
2024-12-16 14:28:48 -0300

    .

--- exercicios_com_modulos/moeda/números/moeda.py
+++ exercicios_com_modulos/moeda/números/moeda.py
@@ -1,22 +1,27 @@
-def metade(valor = 0):
+def metade(valor=0):
     res = valor / 2
     return res
 
-def dobro(valor = 0):
+
+def dobro(valor=0):
     res = valor * 2
     return res
 
-def triplo(valor = 0):
+
+def triplo(valor=0):
     res = valor * 3
     return res
 
-def aumentar(valor = 0, porcentagem = 0):
+
+def aumentar(valor=0, porcentagem=0):
     res = valor + (valor * (porcentagem / 100))
     return res
 
-def diminuir(valor = 0, porcentagem = 0):
+
+def diminuir(valor=0, porcentagem=0):
     res = valor - (valor * (porcentagem / 100))
     return res
 
-def moeda(valor = 0, moeda = 'R$'):
-    return f'{moeda}{valor:.2f}'.replace('.',',')
+
+def moeda(valor=0, moeda='R$'):
+    return f'{moeda}{valor:.2f}'.replace('.', ',')
